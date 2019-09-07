68f065309686b504ec81d1ab4259c4f46b7ae59c
hinfan123 <hinfan123@yahoo.com>
2019-09-06 20:25:55 -0700

    yes

--- shopping.js
+++ shopping.js
@@ -1,17 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems);
 
-    var collapseNav = document.querySelectorAll('.sidenav');
-    var collapse = M.Sidenav.init(collapseNav);
-
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
-});
-$(document).ready(function(){
-  $(".add-row").click(function(){
-      var item = $("#item").val();
-      var type = $("#type").val();
-      $('#myTable tr:last').after('<tr><td>'+item+'</td><td>'+ type +'</td></tr>')
-  });
-)};
